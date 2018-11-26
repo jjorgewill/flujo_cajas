75c1055f69a1cf89ad1a76198e3711103f61baf1
jjorge <jjorgewill@gmail.com>
2018-11-26 18:30:37 -0500

    creando admin

--- apps/flujo/admin.py
+++ apps/flujo/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.flujo import models
+
+
+class MonedaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'pais',)
+    search_fields = ('nombre',)
+    list_filter = ('nombre', 'pais',)
+    ordering = ('pais',)
+
+
+
+class ObligacionesAdmin(admin.ModelAdmin):
+    list_display = ('id','descripcion', 'acredor_obligacion',)
+
+
+admin.site.register(models.Moneda, MonedaAdmin)
+admin.site.register(models.Activo)
+admin.site.register(models.Acredor)
+admin.site.register(models.Obligaciones, ObligacionesAdmin)
+admin.site.register(models.Categoria)
+admin.site.register(models.SubCategoria)
+admin.site.register(models.Movimiento)
--- apps/flujo/models.py
+++ apps/flujo/models.py
@@ -1,44 +1,54 @@
 from django.db import models
 from apps.flujo import properties
 
+
 # Create your models here.
 class Moneda(models.Model):
     pais = models.CharField(max_length=200)
     nombre = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Activo(models.Model):
     nombre_activo = models.CharField(max_length=255)
-    valor_activo = models.DecimalField(decimal_places=2)
-    moneda = models.ForeignKey(Moneda,on_delete=models.CASCADE)
-    tiempo = models.CharField(max_length=30,choices=properties.TIPO_TIEMPO)
-    valor_tiempo =  models.PositiveIntegerField()
+    valor_activo = models.DecimalField(decimal_places=2, max_digits=10)
+    moneda = models.ForeignKey(Moneda, on_delete=models.CASCADE)
+    tiempo = models.CharField(max_length=30, choices=properties.TIPO_TIEMPO)
+    valor_tiempo = models.PositiveIntegerField()
 
 
 class Acredor(models.Model):
     nombre = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Obligaciones(models.Model):
     acredor_obligacion = models.ForeignKey(Acredor, on_delete=models.CASCADE)
-    saldo_obligacion =models.DecimalField()
-    cuota_obligacion = models.DecimalField()
+    saldo_obligacion = models.DecimalField(decimal_places=2, max_digits=10)
+    cuota_obligacion = models.DecimalField(decimal_places=2, max_digits=10)
     tiempo = models.CharField(max_length=30, choices=properties.TIPO_TIEMPO)
     valor_tiempo = models.PositiveIntegerField()
     descripcion = models.CharField(max_length=500)
-    tasa_obligacion = models.DecimalField(decimal_places=2)
+    tasa_obligacion = models.DecimalField(decimal_places=2, max_digits=10, blank=True, null=True)
+
 
 class Categoria(models.Model):
     tipo = models.CharField(max_length=30, choices=properties.TIPO_MOVIMIENTO)
     nombre = models.CharField(max_length=200)
 
+
 class SubCategoria(models.Model):
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE())
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=200)
 
+
 class Movimiento(models.Model):
-    subcategoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE())
+    subcategoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
     fecha = models.DateField()
     nombre_dia = models.CharField(max_length=30, choices=properties.TIPO_DIA_SEMANA)
     nombre = models.CharField(max_length=200)
-    valor = models.DecimalField(decimal_places=2,max_digits=10)
+    valor = models.DecimalField(decimal_places=2, max_digits=10)
