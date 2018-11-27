26107f930d9922c928d09ecb556ab56c98253b59
jjorge <jjorgewill@gmail.com>
2018-11-27 18:09:34 -0500

    css first table list

--- apps/flujo/urls.py
+++ apps/flujo/urls.py
@@ -4,4 +4,6 @@ from apps.flujo import views
 
 urlpatterns = [
     path('home/', views.Home.as_view(), name='view_home'),
+
+    path('activo/listar/', views.ActivoView.as_view(), name='view_activo'),
 ]
--- apps/flujo/views.py
+++ apps/flujo/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
 
+from apps.flujo import models
+
 
 class Home(generic.TemplateView):
     template_name = 'home/login.html'
+
+
+class ActivoView(generic.ListView):
+    template_name = 'activo/listar_activos.html'
+    context_object_name = 'activos'
+    model = models.Activo
+    paginate_by = 10
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
