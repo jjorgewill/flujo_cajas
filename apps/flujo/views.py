f7d598a660cbef54532e46852d905c2357d6ffd4
jjorge <jjorgewill@gmail.com>
2018-11-28 18:32:23 -0500

    paginador

--- apps/flujo/views.py
+++ apps/flujo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
+from django.contrib import messages
 
 from apps.flujo import models, forms
 
@@ -28,9 +29,11 @@ class ActivoCreateView(generic.CreateView):
 
     def form_valid(self, form):
         self.object = form.save()
+        messages.success(self.request, 'Su Activo se ha guardado correctamente.')
         return super().form_valid(form)
 
     def form_invalid(self, form):
+        messages.error(self.request, 'Lo sentimos no se ha podido procesar la solicitud.')
         return self.render_to_response(self.get_context_data(form=form))
 
     def get_success_url(self):
@@ -41,7 +44,7 @@ class ActivoView(generic.ListView):
     template_name = 'activo/listar_activos.html'
     context_object_name = 'activos'
     model = models.Activo
-    paginate_by = 10
+    paginate_by = 2
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
